a755cfaae127e8c757936a07830da9b066262386
amigcamel <amigcamel@gmail.com>
2017-02-03 10:37:59 +0800

    finish: repipients

--- static/js/ajireporter.js
+++ static/js/ajireporter.js
@@ -8,26 +8,29 @@ angular.module('DVReporterApp', ['textAngular', 'angularFileUpload'])
 // }])
 
 .controller('BaseCtrl', ['$scope', 'FileUploader', '$http', function($scope, FileUploader, $http) {
-	$scope.htmlVariable = '<h1 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><b><span lang="EN-US">2016.02.01</span><span>網路輿情摘</span><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><span>要<span lang="EN-US"></span></span></b></h1><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span lang="EN-US">1.</span><span>【各局處】：</span><span>拆忠孝橋引道<span lang="EN-US">2</span>萬多人塞<span lang="EN-US">12</span>分鐘<span lang="EN-US"></span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><b><u><span>社群論壇</span></u></b><span>評價：無相關討論<span lang="EN-US"></span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><b><u><span>新聞網站</span></u></b><span>評價：負評<span lang="EN-US">9</span>＞正評<span lang="EN-US">2</span>（蘋果日報）<span lang="EN-US"></span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><span>網友留言：<span lang="EN-US">(</span>摘要均全文轉貼網友留言<span lang="EN-US">)</span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><span>正評摘要：該拆就要拆<span lang="EN-US"></span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span>負評摘要：</span><span>沒有建設 只會拆除<span lang="EN-US"></span></span></h4><h4 style="color: rgb(85, 85, 85);text-align: center;background-color: rgb(255, 255, 255);"><span>以上報告長官</span></h4>';
+    $scope.settings = {};
+    $scope.settings.htmlVariable = '<h1 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><b><span lang="EN-US">'
+    $scope.settings.htmlVariable += new Date().toJSON().replace(/-/gi, '.').split('T')[0]
+    $scope.settings.htmlVariable += '</span><span>網路輿情摘</span><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><span>要<span lang="EN-US"></span></span></b></h1><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span lang="EN-US">1.</span><span>【     】：</span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><b><u><span>社群論壇</span></u></b><span>評價：<span lang="EN-US"></span></span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><b><u><span>新聞網站</span></u></b><span>評價：<span lang="EN-US"></span></span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span lang="EN-US">*</span><span>網友留言：<span lang="EN-US">(</span>摘要均全文轉貼網友留言<span lang="EN-US">)</span></span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><a rel="nofollow" style="color: rgb(25, 106, 212);background-color: transparent;"></a><span>正評摘要：<span lang="EN-US"></span></span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span>負評摘要：</span></h4><h4 style="text-align: left;color: rgb(85, 85, 85);background-color: rgb(255, 255, 255);"><span>以上報告長官</span></h4>'
     $scope.uploader = new FileUploader({
-    	url: '/uploader/'
+        url: '/uploader/'
     });    
 
-    $scope.hour = 3;
-    $scope.minute = 30;
-    $scope.recipients = [
-    	'amigcamel@gmail.com',
-    	// 'aji@dailyview.tw',
-    	// 'amigcamelowen2@yahoo.com.tw',
-    	// 'ajiliu@mt2015.com',
-    	// 'ami_g_camel@hotmail.com'
-    ];
+    $scope.settings.subject = new Date().toJSON().replace('T', '-').split('-').slice(1,3).join('')+'新北市網路輿情日報表'
+    $scope.settings.hour = 3;
+    $scope.settings.minute = 30;
+    $scope.settings.recipients = [];
+
+    $scope.recipientsEdit = $scope.settings.recipients.join(',\n');
+    $scope.saveRecipients = function() {
+        $scope.settings.recipients = $scope.recipientsEdit.replace(/\n/gi, '').replace(/,$/gi, '').split(',');
+    };
 
     $scope.sendTest = function() {
     	var mail = {
-    		subject: $scope.subject,
-    		recipients: $scope.recipients,
-    		content: $scope.htmlVariable,
+    		subject: $scope.settings.subject,
+    		recipients: $scope.settings.recipients,
+    		content: $scope.settings.htmlVariable,
     	};
     	$http({
     		url: '/sendmail/',
